61acc569d39ffcc453cbb55c1b3248dbdc202189
Leo Kotschenreuther <leo.kotschenreuther@student.hpi.uni-potsdam.de>
2014-10-10 22:49:49 +0200

    Server listens for requests and returns them by calling /echo and giving a request

--- server.py
+++ server.py
@@ -1,13 +1,18 @@
 import os
 
-from flask import Flask
+from flask import Flask, request
 app = Flask(__name__)
 
 
-@app.route('/')
+@app.route('/hello_world', methods=["GET"])
 def hello_world():
     return 'Hello World!'
 
+@app.route('/echo', methods=["GET", "POST"])
+def echo():
+    if request.method == 'POST':
+    	return request.form['test']
+
 if __name__ == "__main__":
     port = os.getenv('VCAP_APP_PORT', '5000')
-    app.run(host='0.0.0.0', port=int(port))
+    app.run(host='0.0.0.0', port=int(port), debug=True)
