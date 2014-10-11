6c666ee90aee3423f0b282501e4ce2719e317936
Fabio Niephaus <fniephaus@users.noreply.github.com>
2014-10-11 12:44:06 +0200

    Add NeoPixel support

--- arduino/HackZurich.ino
+++ arduino/HackZurich.ino
@@ -1,17 +1,25 @@
 #include <Ethernet.h>
 #include <SPI.h>
+#include <Adafruit_NeoPixel.h>
 
-int ledPin = 8;
+int ledPin = 6;
+#define NUMPIXELS      16
 
 byte mac[] = { 0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED };
 
 EthernetClient client;
 
+bool ledsOn = 0;
+
+Adafruit_NeoPixel pixels = Adafruit_NeoPixel(NUMPIXELS, ledPin, NEO_GRB + NEO_KHZ800);
+
 void setup()
 {
   pinMode(ledPin, OUTPUT);
   Serial.begin(9600);
-  
+  pixels.begin();
+  pixels.show();
+    
   while (Ethernet.begin(mac) != 1)
   {
     Serial.println("Error getting IP address via DHCP, trying again...");
@@ -20,8 +28,10 @@ void setup()
   reconnect();
 }
 
+
 void loop()
 {
+  
   char c;
   if (client.available()) {
     c = client.read();
@@ -29,17 +39,40 @@ void loop()
   }
 
   if (!client.connected()) {
+    
     if (c == '1') {
-      digitalWrite(ledPin, HIGH);
-    } else {
-      digitalWrite(ledPin, LOW);
+      if (ledsOn != 1) {
+        for(int j=0; j<150; j++) {
+          for(int i=0;i<NUMPIXELS;i++){
+            pixels.setPixelColor(i, pixels.Color(0,j,0));
+          }
+          pixels.show();
+          delay(5);
+        }
+        ledsOn = 1;
+      }
+      // Serial.println("on");
+    } else if (c == '0') {
+      if (ledsOn != 0) {
+        for(int j=0; j<150; j++) {
+          for(int i=0;i<NUMPIXELS;i++){
+            pixels.setPixelColor(i, pixels.Color(0,150-j,0));
+          }
+          pixels.show();
+          delay(5);
+        }
+        ledsOn = 0;
+      }
+      // Serial.println("off");
     }
     // Serial.println("disconnecting.");
     client.stop();
-    delay(100);
+    delay(200);
     reconnect();
   }
 }
+
+
 
 void reconnect()
 {
