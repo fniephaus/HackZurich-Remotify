bf99f1f773100f7b45b1f1d87deca241ca53982e
Fabio Niephaus <fniephaus@users.noreply.github.com>
2014-10-12 09:25:03 +0200

    Minor change

--- osx/osx.py
+++ osx/osx.py
@@ -6,7 +6,7 @@ iTunes = app('iTunes')
 
 while True:
     try:
-        state = urllib2.urlopen("http://hackzurich.beta.scapp.io/pc/status").read()
+        state = urllib2.urlopen("http://hackzurich.beta.scapp.io/mac/status").read()
         if state == '1':
             iTunes.next_track()
         elif state == '2':
